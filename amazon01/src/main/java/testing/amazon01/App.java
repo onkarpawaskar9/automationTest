734c94af26e64cc1adc94b99158610c869804b5d
Ashwini Salunkhe <ashwinirsalunkhe@gmail.com>
2022-04-23 16:03:05 +0530

    task001

--- amazon01/src/main/java/testing/amazon01/App.java
+++ amazon01/src/main/java/testing/amazon01/App.java
@@ -1,13 +1,25 @@
 package testing.amazon01;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+        System.setProperty("webdriver.chrome.driver","\\Users\\Vijay Thorat\\Downloads\\chromedriver.exe");		
+		
+    	WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in/");
+		
+		driver.findElement(By.xpath("/html/body/div[1]/header/div/div[4]/div[2]/div[2]/div/a[2]")).click();
+        
+		driver.manage().window().maximize();
+        		
+    
     }
 }
+
