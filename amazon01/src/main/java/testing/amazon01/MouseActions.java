f060f843d8fd1ee1c4869969875707428053115f
Ashwini Salunkhe <ashwinirsalunkhe@gmail.com>
2022-04-26 16:24:39 +0530

    MouseActionsModified

--- amazon01/src/main/java/testing/amazon01/MouseActions.java
+++ amazon01/src/main/java/testing/amazon01/MouseActions.java
@@ -3,7 +3,9 @@ package testing.amazon01;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class MouseActions {
 
@@ -13,8 +15,16 @@ public class MouseActions {
 		System.setProperty("webdriver.chrome.driver","\\Users\\Vijay Thorat\\Downloads\\chromedriver.exe");		
 		
     	WebDriver driver = new ChromeDriver();
+    	
+    	driver.manage().window().maximize();
 		
-		driver.get("https://demoqa.com/alerts");
+    	driver.get("https://www.browserstack.com/");
+    	Actions action = new Actions(driver); 
+    	
+    	
+    	WebElement element = driver.findElement(By.linkText("Get started free"));
+
+    	action.moveToElement(element).click().perform();
 	}
 
 }
