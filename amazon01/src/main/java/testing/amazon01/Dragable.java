ff0bbb59f6aa588913b55ea4deef4ca3aaeea2f5
onkarpawaskar9 <onkarpawaskkar9@gmail.com>
2022-04-26 22:54:41 +0530

    change in Dragable file

--- amazon01/src/main/java/testing/amazon01/Dragable.java
+++ amazon01/src/main/java/testing/amazon01/Dragable.java
@@ -1,9 +1,18 @@
 package testing.amazon01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Dragable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","\\Users\\Vijay Thorat\\Downloads\\chromedriver.exe");		
+		
+    	WebDriver driver = new ChromeDriver();
+    	
+    	driver.manage().window().maximize();
+		
+    	driver.get("https://www.browserstack.com/");
 
 	}
 
