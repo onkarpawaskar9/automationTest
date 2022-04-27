a310bec021d00d73de0972b9714945e2b6183a70
onkarpawaskar9 <onkarpawaskkar9@gmail.com>
2022-04-27 18:18:19 +0530

    modified Dragable

--- amazon01/src/main/java/testing/amazon01/App.java
+++ amazon01/src/main/java/testing/amazon01/App.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App 
 {
+	
 	public static void main( String[] args )
     {
     	
--- amazon01/src/main/java/testing/amazon01/Dragable.java
+++ amazon01/src/main/java/testing/amazon01/Dragable.java
@@ -1,19 +1,40 @@
 package testing.amazon01;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import io.opentelemetry.sdk.logs.data.Body;
 
 public class Dragable {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","\\Users\\Vijay Thorat\\Downloads\\chromedriver.exe");		
 		
     	WebDriver driver = new ChromeDriver();
     	
     	driver.manage().window().maximize();
 		
-    	driver.get("https://www.browserstack.com/");
+    	driver.get("https://demoqa.com/droppable");
+    	
+    	WebElement fromElement = driver.findElement(By.id("draggable"));
 
+    	//WebElement to which the above object is dropped
+    	WebElement toElement = driver.findElement(By.id("droppable"));
+
+    	//Creating object of Actions class to build composite actions
+    	Actions builder = new Actions(driver);
+
+    	//Building a drag and drop action
+    	Action dragAndDrop = builder.clickAndHold(fromElement)
+    	.moveToElement(toElement)
+    	.release(toElement)
+    	.build();
+
+    	//Performing the drag and drop action
+    	dragAndDrop.perform();
 	}
 
 }
